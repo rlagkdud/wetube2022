3f447b683972707f2cfe2b3eea342a68ee8879fe
rlagkdud <ha0936861@gmail.com>
2022-02-09 19:03:31 +0900

    middleware example

--- src/server.js
+++ src/server.js
@@ -4,14 +4,19 @@ const PORT = 4000;
 
 const app = express();
 //sandwitch - express앱이 만들어지고 난 후에 작성해야하는 코드임.
+
+const gossipMiddleware = (req, res, next) => {
+    console.log(`someone is goint to : ${req.url}~!!`);
+    next();
+}
 const handleHome = (req, res) => {
-    return res.send("I still love you")
+    return res.send("<h1>I still love you<h1>")
 };
 const handleLogin = (req, res) => {
-    return res.send("Login here");
+    return res.send({ message: "Login here" });
 }
 
-app.get("/", handleHome);
+app.get("/", gossipMiddleware, handleHome);
 app.get("/login", handleLogin);
 
 const handleListening = () => console.log(`✅ Server listening on http://localhost:${PORT} 🚀`);
