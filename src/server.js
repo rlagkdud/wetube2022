5cc9c5ed0ada085f936dbb9cd835ace5e747b9be
rlagkdud <ha0936861@gmail.com>
2022-02-10 18:54:33 +0900

    add morgan (return middleware)

--- src/server.js
+++ src/server.js
@@ -1,23 +1,29 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
 //sandwitch - express앱이 만들어지고 난 후에 작성해야하는 코드임.
 
-const gossipMiddleware = (req, res, next) => {
-    console.log(`someone is goint to : ${req.url}~!!`);
-    next();
-}
 const handleHome = (req, res) => {
     return res.send("<h1>I still love you<h1>")
 };
 const handleLogin = (req, res) => {
     return res.send({ message: "Login here" });
-}
+};
 
-app.get("/", gossipMiddleware, handleHome);
+const handleProtected = (req, res) => {
+    return res.send("Welcome to the private lounge");
+};
+
+app.use(logger);
+
+app.get("/", handleHome);
 app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
+
 
 const handleListening = () => console.log(`✅ Server listening on http://localhost:${PORT} 🚀`);
 
