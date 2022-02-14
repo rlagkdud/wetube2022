e2f9279621e306cd3ccbda7566e7d7105ec85784
rlagkdud <ha0936861@gmail.com>
2022-02-14 19:48:28 +0900

    add parameter(id: only digit)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,7 +1,13 @@
 export const trending = (req,res) =>  res.send("homepage Video~!!");
 export const search = (req, res) => res.send("Search~!");
 
-export const see = (req, res) => res.send("Video Watch!!");
-export const edit = (req, res) => res.send("Video Edit~!");
-export const deleteVideo = (req, res) => res.send("delete video!")
+export const see = (req, res) => {
+    return res.send(`Watch video #${req.params.id}`);
+}
+export const edit = (req, res) => {
+    return res.send("Video Edit~!");
+}
+export const deleteVideo = (req, res) => {
+    return res.send("delete video!")
+} 
 export const upload = (req, res) => res.send("upload video!")
