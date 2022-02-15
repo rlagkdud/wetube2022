5ccf2e615539115c95f27a21af10ed728b8ad263
rlagkdud <ha0936861@gmail.com>
2022-02-15 17:20:50 +0900

    add javascript to template

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,7 @@
 export const trending = (req,res) =>  res.render("home");
 export const search = (req, res) => res.send("Search~!");
 
-export const see = (req, res) => {
-    return res.send(`Watch video #${req.params.id}`);
-}
+export const see = (req, res) => res.render("watch");
 export const edit = (req, res) => {
     return res.send("Video Edit~!");
 }
