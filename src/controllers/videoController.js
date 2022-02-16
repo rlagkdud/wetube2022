79ff0a10094f1bf6e8b635abec115485b3711b21
rlagkdud <ha0936861@gmail.com>
2022-02-16 12:57:23 +0900

    partials - footer

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -2,9 +2,7 @@ export const trending = (req,res) =>  res.render("home");
 export const search = (req, res) => res.send("Search~!");
 
 export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => {
-    return res.send("Video Edit~!");
-}
+export const edit = (req, res) => res.render("edit");
 export const deleteVideo = (req, res) => {
     return res.send("delete video!")
 } 
