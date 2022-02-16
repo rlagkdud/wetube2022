2f8e4ac4e03750fdbab791189a8f270c7fc1c6cf
rlagkdud <ha0936861@gmail.com>
2022-02-16 16:52:25 +0900

    add MVP.css to show better HTML

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,8 @@
 export const trending = (req,res) =>  res.render("home", {pageTitle: "Home"});
 export const search = (req, res) => res.send("Search~!");
 
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
+export const see = (req, res) => res.render("watch",{pageTitle:"Watch Video"});
+export const edit = (req, res) => res.render("edit",{pageTitle: "Edit Video"});
 export const deleteVideo = (req, res) => {
     return res.send("delete video!")
 } 
