02f4599fe877a074c73cc60b7666daa2cc07a7c4
rlagkdud <ha0936861@gmail.com>
2022-02-17 19:06:59 +0900

    postEdit with fake database

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -32,7 +32,6 @@ export const search = (req, res) => res.send("Search~!");
 
 export const watch = (req, res) => {
     const { id } = req.params;
-    console.log("show video", id);
     const video = videos[id-1];
     return res.render("watch",{pageTitle:`Watching: ${video.title}` , video});
 }
@@ -42,5 +41,8 @@ export const getEdit = (req, res) => {
     return res.render("edit",{pageTitle: `Editing: ${video.title}`, video});
 }
 export const postEdit = (req, res) => {
-    
+    const { id } = req.params;
+    const {title} = req.body;
+    videos[id-1].title = title;
+    return res.redirect(`/videos/${id}`);
 }
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -4,8 +4,7 @@ import { watch,postEdit, getEdit  } from "../controllers/videoController";
 const videoRouter = express.Router();
 
 videoRouter.get("/:id(\\d+)", watch);
-videoRouter.get("/:id(\\d+)/edit",getEdit ); 
-videoRouter.post("/:id(\\d+)/edit", postEdit); 
+videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
 
 
 
