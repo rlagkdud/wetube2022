8b493dd027f51e8362bb20370e368921fac53069
rlagkdud <ha0936861@gmail.com>
2022-03-03 21:16:48 +0900

    join

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,17 @@
-export const join = (req, res) => res.send("Join~!!!");
+import User from "../models/User";
+
+export const getJoin = (req, res) => res.render("join",{pageTitle: "Join" });
+export const postJoin = async (req, res) => {
+    const {name, email, userName, password, location} = req.body;
+    await User.create({
+        name, 
+        userName,
+        email,
+        password, 
+        location,
+    });
+    return res.redirect("/login")
+};
 export const login = (req, res) => res.send("Log in!");
 
 export const edit = (req, res) => res.send("User Edit!");
