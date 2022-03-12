f759eb91d3b7d1e70f7fe78aae440b587540e3c3
rlagkdud <ha0936861@gmail.com>
2022-03-12 21:57:32 +0900

    set expire time of cookie

--- src/server.js
+++ src/server.js
@@ -20,6 +20,9 @@ app.use(session({
     secret:"Hello!",
     resave: false,
     saveUninitialized: false,
+    cookie: {
+        maxAge: 20000,
+    },
     store: MongoStore.create({mongoUrl: "mongodb://127.0.0.1:27017/wetube"}),
 }));
 
