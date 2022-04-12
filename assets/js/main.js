f51ba48bf675ab3a1e92074a3c807043e0f9e043
rlagkdud <ha0936861@gmail.com>
2022-04-12 21:28:36 +0900

    play and pause video in fucntion

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,16 +7,15 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/client/scss/styles.scss":
 /*!*************************************!*\
   !*** ./src/client/scss/styles.scss ***!
   \*************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (() => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://wetube2022/./src/client/scss/styles.scss?");
+eval("throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nHookWebpackError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nSassError: expected \\\")\\\".\\n   ╷\\n12 │ input:not(input(type=\\\"range\\\")){\\n   │                ^\\n   ╵\\n  src/client/scss/components/forms.scss 12:16  @import\\n  src/client/scss/styles.scss 10:9             root stylesheet\\n    at tryRunOrWebpackError (/Users/hayeongkim/Desktop/wetube2022/node_modules/webpack/lib/HookWebpackError.js:88:9)\\n    at __webpack_require_module__ (/Users/hayeongkim/Desktop/wetube2022/node_modules/webpack/lib/Compilation.js:5051:12)\\n    at __webpack_require__ (/Users/hayeongkim/Desktop/wetube2022/node_modules/webpack/lib/Compilation.js:5008:18)\\n    at /Users/hayeongkim/Desktop/wetube2022/node_modules/webpack/lib/Compilation.js:5079:20\\n    at symbolIterator (/Users/hayeongkim/Desktop/wetube2022/node_modules/neo-async/async.js:3485:9)\\n    at done (/Users/hayeongkim/Desktop/wetube2022/node_modules/neo-async/async.js:3527:9)\\n    at Hook.eval [as callAsync] (eval at create (/Users/hayeongkim/Desktop/wetube2022/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:15:1)\\n    at Hook.CALL_ASYNC_DELEGATE [as _callAsync] (/Users/hayeongkim/Desktop/wetube2022/node_modules/tapable/lib/Hook.js:18:14)\\n    at /Users/hayeongkim/Desktop/wetube2022/node_modules/webpack/lib/Compilation.js:4986:43\\n    at symbolIterator (/Users/hayeongkim/Desktop/wetube2022/node_modules/neo-async/async.js:3482:9)\\n-- inner error --\\nError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nSassError: expected \\\")\\\".\\n   ╷\\n12 │ input:not(input(type=\\\"range\\\")){\\n   │                ^\\n   ╵\\n  src/client/scss/components/forms.scss 12:16  @import\\n  src/client/scss/styles.scss 10:9             root stylesheet\\n    at Object.<anonymous> (/Users/hayeongkim/Desktop/wetube2022/node_modules/css-loader/dist/cjs.js!/Users/hayeongkim/Desktop/wetube2022/node_modules/sass-loader/dist/cjs.js!/Users/hayeongkim/Desktop/wetube2022/src/client/scss/styles.scss:1:7)\\n    at /Users/hayeongkim/Desktop/wetube2022/node_modules/webpack/lib/javascript/JavascriptModulesPlugin.js:441:11\\n    at Hook.eval [as call] (eval at create (/Users/hayeongkim/Desktop/wetube2022/node_modules/tapable/lib/HookCodeFactory.js:19:10), <anonymous>:7:1)\\n    at Hook.CALL_DELEGATE [as _call] (/Users/hayeongkim/Desktop/wetube2022/node_modules/tapable/lib/Hook.js:14:14)\\n    at /Users/hayeongkim/Desktop/wetube2022/node_modules/webpack/lib/Compilation.js:5053:39\\n    at tryRunOrWebpackError (/Users/hayeongkim/Desktop/wetube2022/node_modules/webpack/lib/HookWebpackError.js:83:7)\\n    at __webpack_require_module__ (/Users/hayeongkim/Desktop/wetube2022/node_modules/webpack/lib/Compilation.js:5051:12)\\n    at __webpack_require__ (/Users/hayeongkim/Desktop/wetube2022/node_modules/webpack/lib/Compilation.js:5008:18)\\n    at /Users/hayeongkim/Desktop/wetube2022/node_modules/webpack/lib/Compilation.js:5079:20\\n    at symbolIterator (/Users/hayeongkim/Desktop/wetube2022/node_modules/neo-async/async.js:3485:9)\\n\\nGenerated code for /Users/hayeongkim/Desktop/wetube2022/node_modules/css-loader/dist/cjs.js!/Users/hayeongkim/Desktop/wetube2022/node_modules/sass-loader/dist/cjs.js!/Users/hayeongkim/Desktop/wetube2022/src/client/scss/styles.scss\\n1 | throw new Error(\\\"Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\\\nSassError: expected \\\\\\\")\\\\\\\".\\\\n   ╷\\\\n12 │ input:not(input(type=\\\\\\\"range\\\\\\\")){\\\\n   │                ^\\\\n   ╵\\\\n  src/client/scss/components/forms.scss 12:16  @import\\\\n  src/client/scss/styles.scss 10:9             root stylesheet\\\");\");\n\n//# sourceURL=webpack://wetube2022/./src/client/scss/styles.scss?");
 
 /***/ }),
 
@@ -26,6 +25,7 @@ eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extr
   \*******************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+"use strict";
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _scss_styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../scss/styles.scss */ \"./src/client/scss/styles.scss\");\n\nconsole.log(\"main\");\n\n//# sourceURL=webpack://wetube2022/./src/client/js/main.js?");
 
 /***/ })
--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,24 @@
 console.log("video player");
+
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlay = (c) => {
+  // if the video is playing, pause it
+  if (video.paused) {
+    playBtn.innerHTML = "Pause";
+    video.play();
+  } else {
+    playBtn.innerText = "Play";
+    video.pause();
+  }
+  // else play the video
+};
+
+const handleMute = (c) => {};
+
+playBtn.addEventListener("click", handlePlay);
+muteBtn.addEventListener("click", handlePlay);
