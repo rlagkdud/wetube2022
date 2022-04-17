298cd9c0430d2ed1b70e54ee4646bf0321af0aa7
rlagkdud <ha0936861@gmail.com>
2022-04-17 21:37:24 +0900

    full screen

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -7,6 +7,8 @@ const volumeRange = document.getElementById("volume");
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
 const timeline = document.getElementById("timeline");
+const fullScreen = document.getElementById("fullScreen");
+const videoContainer = document.getElementById("videoContainer");
 
 let volumeValue = 0.5;
 video.volume = volumeValue;
@@ -62,9 +64,21 @@ const handleTimelineChange = (event) => {
   video.currentTime = value;
 };
 
+const handleFullScreen = () => {
+  const fullscreen = document.fullscreenElement;
+  if (fullscreen) {
+    document.exitFullscreen();
+    fullScreen.innerText = "Enter full screen";
+  } else {
+    videoContainer.requestFullscreen();
+    fullScreen.innerText = "Exit full screen";
+  }
+};
+
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeChange);
 video.addEventListener("loadedmetadata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
 timeline.addEventListener("input", handleTimelineChange);
+fullScreen.addEventListener("click", handleFullScreen);
