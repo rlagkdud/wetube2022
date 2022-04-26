c81091996ad801b59833aff6df131daff83c2abf
rlagkdud <ha0936861@gmail.com>
2022-04-26 21:30:44 +0900

    record video

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,13 +1,40 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+
+const handleStop = () => {
+  startBtn.innerText = "Start Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleStart);
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+
+  const recorder = new MediaRecorder(stream);
+  console.log(recorder);
+  recorder.ondataavailable = (e) => {
+    console.log("recording done");
+    console.log(e);
+    console.log(e.data);
+  };
+  recorder.start();
+  console.log(recorder);
+  setTimeout(() => {
+    recorder.stop();
+  }, 10000);
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
-    video: { width: 200, height: 100 },
+    video: true,
   });
   video.srcObject = stream;
   video.play();
 };
-
+init();
 startBtn.addEventListener("click", handleStart);
