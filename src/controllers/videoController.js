539b9dfa96537ca79e77467d2757b02c1166287d
rlagkdud <ha0936861@gmail.com>
2022-05-11 23:34:35 +0900

    send stringify object from frontend and get json object in backend

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -136,5 +136,6 @@ export const registerView = async (req, res) => {
 export const createComment = (req, res) => {
   console.log(req.params);
   console.log(req.body);
+  console.log(req.body.text);
   return res.end();
 };
