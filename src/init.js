f96e016014c433d2d3f7d4d1fe5b731cab217ab0
rlagkdud <ha0936861@gmail.com>
2022-05-18 00:27:16 +0900

    PORT env variable

--- src/init.js
+++ src/init.js
@@ -6,7 +6,7 @@ import "./models/User";
 import "./models/Comment";
 import app from "./server";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000; //process.env.PORT: Heroku give to me.
 
 const handleListening = () =>
   console.log(`✅ Server listening on http://localhost:${PORT} 🚀`);
