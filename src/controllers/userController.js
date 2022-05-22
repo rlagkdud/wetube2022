f07bfbd3ee74d438c18f51006344e0366263a79c
rlagkdud <ha0936861@gmail.com>
2022-05-22 21:58:37 +0900

    add isheroku

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -160,10 +160,11 @@ export const postEdit = async (req, res) => {
       errorMessage: "User exists already",
     });
   }
+  const isheroku = process.env.NODE_ENV === "production";
   const updatedUser = await User.findByIdAndUpdate(
     _id,
     {
-      avatarUrl: file ? file.location : avatarUrl,
+      avatarUrl: file ? (isheroku ? file.location : file.path) : avatarUrl,
       name,
       email,
       userName,
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -70,14 +70,15 @@ export const postUpload = async (req, res) => {
     user: { _id },
   } = req.session;
   const { video, thumb } = req.files;
-
   const { title, description, hashtags } = req.body;
+  const isheroku = process.env.NODE_ENV === "production";
+
   try {
     const newVideo = await Video.create({
       title,
       description,
-      fileUrl: video[0].location,
-      thumbUrl: thumb[0].location,
+      fileUrl: isheroku ? video[0].location : video[0].path,
+      thumbUrl: isheroku ? thumb[0].location : video[0].path,
       owner: _id,
       hashtags: Video.formatHashtags(hashtags),
     });
